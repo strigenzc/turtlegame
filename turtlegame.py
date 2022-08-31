b29077c6d2f3883cc1c0bc98ca92902d48593bc7
Catie Strigenz <97646601+strigenzc@users.noreply.github.com>
2022-08-31 14:01:00 +1000

    Adding turning function to turtle

--- turtlegame.py
+++ turtlegame.py
@@ -1,5 +1,4 @@
 #building a turtle game via She Codes Aus Tut - Space Turtle Chomp
-
 import turtle
 
 # Set up screen
@@ -16,5 +15,10 @@ player.penup()
 # Set speed variable
 speed = 1
 
+# Set keyboard binding
+turtle.listen()
+turtle.onkey(turn_left, 'Left')
+turtle.onkey(turn_right, 'Right')
+turtle.onkey(increase_speed, 'Up')
 while True:
     player.forward(speed)
