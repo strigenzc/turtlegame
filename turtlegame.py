4f1d86191d57e50ce770b8c41b97bbc9e0c26e35
Catie Strigenz <97646601+strigenzc@users.noreply.github.com>
2022-08-31 14:15:42 +1000

    Debugged

--- turtlegame.py
+++ turtlegame.py
@@ -1,24 +1,45 @@
 #building a turtle game via She Codes Aus Tut - Space Turtle Chomp
 import turtle
 
-# Set up screen
+#Set up screen
 turtle.setup(650,650)
 wn = turtle.Screen()
-wn.bgcolor('mediumslateblue')
+wn.bgcolor("mediumslateblue")
 
-# Create player turtle
+
+#Create player turtle
 player = turtle.Turtle()
-player.color('coral')
-player.shape('turtle')
+player.color("coral")
+player.shape("turtle")
 player.penup()
+player.speed(0) 
 
-# Set speed variable
+#Set speed variable
 speed = 1
 
-# Set keyboard binding
+#Define  functions
+def turn_left():
+    player.left(30)
+
+def turn_right():
+    player.right(30)
+
+def increase_speed():
+    global speed
+    speed += 1
+
+def decrease_speed():
+    global speed
+    speed -= 1
+
+#Set keyboard bindings
 turtle.listen()
-turtle.onkey(turn_left, 'Left')
-turtle.onkey(turn_right, 'Right')
-turtle.onkey(increase_speed, 'Up')
+turtle.onkey(turn_left, "Left")
+turtle.onkey(turn_right, "Right")
+turtle.onkey(increase_speed, "Up") 
+turtle.onkey(decrease_speed, "Down") 
+
+
+
 while True:
     player.forward(speed)
